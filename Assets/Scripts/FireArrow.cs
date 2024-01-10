54a5610ce3609d32d718ca4d191d553222e082fa
MTBAce <elliott.thiemeforslund@elev.ga.lbs.se>
2024-01-10 16:28:13 +0100

    Worked on character movement etc
    Added arm movement, Crossbow

---
+++ Assets/Scripts/FireArrow.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FireArrow : MonoBehaviour
+{
+    public Transform arrowSpawn;
+    public GameObject BulletPrefab;
+    public float arrowSpeed = 10;
+
+    public float shootDelay = 1.0f;
+    public float timeSinceLastShoot = 0.0f;
+
+    void Update()
+    {
+
+        timeSinceLastShoot += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && timeSinceLastShoot >= shootDelay)
+        {
+            var bullet = Instantiate(BulletPrefab, arrowSpawn.position, arrowSpawn.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = arrowSpawn.right * arrowSpeed;
+
+            timeSinceLastShoot = 0.0f;
+        }
+    }
+}
