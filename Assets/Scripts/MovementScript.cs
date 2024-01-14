59bbbca53781616d9a50e5c08e61b1a6685e77fc
Hampus Carlström Svanberg <150423568+Hampus-CS@users.noreply.github.com>
2024-01-14 22:19:53 +0100

    Arm rotation and flip (#11)

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -8,7 +8,7 @@ public class Movementscript : MonoBehaviour
     private float horizontal;
     public float jumpingPower = 10;
     public float speed = 0f;
-    public bool isFacingRight;
+    public bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -22,9 +22,17 @@ public class Movementscript : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
+        if (horizontal < 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (horizontal > 0 && isFacingRight)
+        {
+            Flip();
+        }
+
     }
-
-
+    
     private bool isGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
@@ -34,5 +42,12 @@ public class Movementscript : MonoBehaviour
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
-
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+    
 }
