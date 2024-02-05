ff1523f7208f955daf789067bb3369abe236646b
GustavThunberg <154264228+Gustav-t@users.noreply.github.com>
2024-02-05 20:16:47 +0100

    Fixade alla variablar i ui och nivåerna

--- Assets/Scripts/Win3.cs
+++ Assets/Scripts/Win3.cs
@@ -5,14 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Win3 : MonoBehaviour
 {
-
+    public GameObject WinScreen;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level4");
+            WinScreen.SetActive(true);
         }
     }
-} 
-
+}
 
--- Assets/Scripts/Win4.cs
+++ Assets/Scripts/Win4.cs
@@ -5,14 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Win4 : MonoBehaviour
 {
-
+    public GameObject WinScreen;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("WinScreen");
+            WinScreen.SetActive(true);
         }
     }
-} 
-
+}
 
